ddc8459fc0ab41d80789cbbf246f851aebf6146d
Adam Morelli <amorelli@gmail.com>
2018-06-25 21:21:35 -0400

    Minor code structure change.

--- server.js
+++ server.js
@@ -48,15 +48,15 @@ app.get('/about', (request, response) => {
 	});
 });
 
-app.get('/bad', (request, response) => {
-	response.send({
-		errorMessage: 'Unable to handle request'
-	});
-});
-
 app.get('/projects', (request, response) => {
 	response.render('projects.hbs', {
 		pageTitle: 'Projects Page'
+	});
+});
+
+app.get('/bad', (request, response) => {
+	response.send({
+		errorMessage: 'Unable to handle request'
 	});
 });
 
